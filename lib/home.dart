a4fe05da1ab230e229c00e340b0db7b9748eff4e
Rafeh Hameed <rhameed@students.uit.edu>
2021-12-01 04:22:54 +0500

    Day 3 Completed

---
+++ lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final days = 30;
+  final name = "Rafeh";
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("$days days of Coding being done by $name."),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
