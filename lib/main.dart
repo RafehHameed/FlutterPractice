959bf602bb3d60350a5ba261ac2932e35f77e7da
Rafeh Hameed <rhameed@students.uit.edu>
2021-12-01 23:05:15 +0500

    Day4

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.amber),
+      theme: ThemeData(
+          primarySwatch: Colors.amber,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          primaryTextTheme: GoogleFonts.latoTextTheme()),
       routes: {
         "/home": (context) => HomePage(),
         "/": (context) => LoginPage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,17 +3,53 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
+  LoginPage({Key? key}) : super(key: key);
+  final mycontroller = TextEditingController();
+  final mycontroller1 = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold),
-        ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/undraw_Mobile_login_re_9ntv.png",
+            fit: BoxFit.cover,
+            height: 200,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 40),
+            child: Column(children: [
+              TextFormField(
+                controller: mycontroller,
+                decoration: InputDecoration(
+                    hintText: "Enter UserName", labelText: "UserName"),
+              ),
+              TextFormField(
+                controller: mycontroller1,
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Enter Password", labelText: "Password"),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                child: Text("Login"),
+                style: TextButton.styleFrom(),
+                onPressed: () {
+                  print(
+                      "Hi ${mycontroller.text} with password ${mycontroller1.text}");
+                },
+              ),
+            ]),
+          ),
+        ],
       ),
     );
   }
